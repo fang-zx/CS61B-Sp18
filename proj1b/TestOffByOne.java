c2de285b5d21fce260d59183dfc3564c2be90d84
fangzx <fangzx@zju.edu.cn>
2020-09-03 08:21:53 +0800

    add more test cases

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -16,5 +16,7 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('A', 'b'));
+        assertFalse(offByOne.equalChars('a', 'B'));
     }
 }
